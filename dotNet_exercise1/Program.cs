6f54d437286d18f53582491b7846a5559003cb9d
Midhat232 <92824796+Midhat232@users.noreply.github.com>
2021-11-29 22:07:55 +0530

    Add files via upload

---
+++ dotNet_exercise1/Program.cs
@@ -0,0 +1,16 @@
+﻿// exercise1
+  //console.WriteLine("Hello World!"); - wrong statement
+  // Console.WriteLine('Hello World!'); - wrong statement 
+  // Console.WriteLine("Hello World!");
+
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code!");
+
+//exercise2
+Console.WriteLine("This is the first line.");
+
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
+
