d43cb5e59664beb073faaf3c363d0c014b96c7c7
Midhat232 <92824796+Midhat232@users.noreply.github.com>
2021-11-30 16:53:52 +0530

    Add files via upload

---
+++ Demo_dotNet/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Demo_dotNet // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This is a stats world!");
+            int[] a = { 40, 38, 37, 36, 34, 33, 32, 31, 30, 30, 30, 29, 26, 26, 19 };
+            int n = a.Length;
+            // Function call
+            Console.WriteLine("Mean = " + stats.findMean(a, n) + "\n");
+            Console.WriteLine("Median = " + stats.findMedian(a, n) + "\n");
+            Console.WriteLine("Mode = " + stats.findMode(a, n) + "\n");
+
+        }
+    }
+}
---
+++ Demo_dotNet/stats.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_dotNet
+{
+    internal class stats
+    {
+        public static double findMean(int[] a, int n)
+        {
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+                sum += a[i];
+
+            return (double)sum / (double)n;
+        }
+        // Function for
+        // calculating median
+        public static double findMedian(int[] a, int n)
+        {
+            // First we sort
+            // the array
+            Array.Sort(a);
+
+            // check for
+            // even case
+            if (n % 2 != 0)
+                return (double)a[n / 2];
+
+            return (double)(a[(n - 1) / 2] + a[n / 2]) / 2.0;
+        }
+        public static double findMode(int[] a,int n)
+        {
+            int maxValue=0, maxCount=0;
+
+            for (int i = 0; i <n; ++i)
+            {
+                int count = 0;
+                for (int j = 0; j < n; ++j)
+                {
+                    if (a[j] == a[i]) 
+                        ++count;
+                }
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                    maxValue = a[i];
+                }
+            }
+
+            return maxValue;
+        }
+    }
+}
